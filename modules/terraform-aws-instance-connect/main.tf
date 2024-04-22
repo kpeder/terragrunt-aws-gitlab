26378334b83c4239320fdfcd30f3814d7aecdaf0
Kevin Pedersen <kpedersen@pythian.com>
2024-04-22 09:13:22 -0300

    basic instance connect module

--- modules/terraform-aws-instance-connect/main.tf
+++ modules/terraform-aws-instance-connect/main.tf
@@ -1,4 +1,6 @@
 resource "aws_ec2_instance_connect_endpoint" "this" {
-  subnet_id = var.subnet_id
-  tags      = var.tags
+  preserve_client_ip = var.preserve_client_ip
+  security_group_ids = var.security_group_ids
+  subnet_id          = var.subnet_id
+  tags               = var.tags
 }
--- modules/terraform-aws-instance-connect/variables.tf
+++ modules/terraform-aws-instance-connect/variables.tf
@@ -1,3 +1,17 @@
+variable "preserve_client_ip" {
+    description = "Whether to preserve client source addresses during connection forwarding."
+    type        = bool
+
+    default = true
+}
+
+variable "security_group_ids" {
+    description = "List of security group ids to associate with the instance connect endpoint."
+    type        = list(string)
+
+    default = []
+}
+
 variable "subnet_id" {
     description = "Id of the subnet in which to deploy the instance connect endpoint."
     type        = string
